669db572ea1907201331127e5f815168432be2f7
Viktor Atsberher <143943277+vicberger7@users.noreply.github.com>
2023-12-11 09:44:48 +0100

    hw-ff

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,1 @@
-function isEnoughCapacity(products, containerSize) {
-    let totalQuantity = 0;
-  
-    for (let key in products) {
-      totalQuantity += products[key];
-    }
-    
-    return totalQuantity <= containerSize;
-}
-
+1234
