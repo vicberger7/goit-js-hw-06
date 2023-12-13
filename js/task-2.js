4b4ec5de844fbc4b04812723a39f247804b75f48
Viktor Atsberher <143943277+vicberger7@users.noreply.github.com>
2023-12-13 10:45:22 +0100

    hw-hl3

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,22 @@
 class Storage {
-  #item;
+  #items;
 
   constructor(items) {
-    this.#item = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this.#item;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.#item.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    const index = this.#item.indexOf(itemToRemove);
+    const index = this.#items.indexOf(itemToRemove);
     if (index > -1) {
-      this.#item.splice(index, 1);
+      this.#items.splice(index, 1);
     }
   }
 }
